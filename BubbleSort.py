7dc95516e563cbb14fb6da6a831ae18aad1eda96
sharan376002 <sharang.developer@gmail.com>
2024-11-18 00:24:29 +0530

    new code added

---
+++ BubbleSort.py
@@ -0,0 +1,19 @@
+# bubble Sort
+
+# IT compare the Rwo Values and then Swaps 
+
+arr = [1,7,8,86,98,99,343,2,4,5,65,6,12,11,13]
+
+for i in range(0,len(arr)-1):
+    for j in range(0,len(arr)-1):
+        if arr[j]<arr[j+1]:
+            continue
+
+        elif arr[j]>arr[j+1]:
+          #  arr[j],arr[j+1] = arr[j+1],arr[i]  # also we can do the swaping operation using a temp variabl'
+            #'''
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+           # '''
+print(arr)
