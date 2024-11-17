a6c0ecb99e69205cc57b8839edeb501eeb87182c
sharan376002 <sharang.developer@gmail.com>
2024-11-17 23:38:05 +0530

    new code

---
+++ LinearSearch.py
@@ -0,0 +1,13 @@
+# linear Search
+# it return th eidex of the element
+
+arr = [1,2,3,4,8,9,77]
+
+searching_element = 77
+
+for i in  range(0,len(arr)):
+   if arr[i] == searching_element:
+     print(i," in the searching element ")
+     break
+else:
+   print("Searching Element not Found !! ")     
