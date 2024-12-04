75341d508da27b8eb29de8a7faa74ca7946a3172
sharan376002 <sharang.developer@gmail.com>
2024-12-04 23:16:48 +0530

    linkedlist

--- linkedlistOperation.py
+++ linkedlistOperation.py
@@ -6,4 +6,15 @@ class node:
 
 class linkedlistOpertation:
     def __init__(self):
-        self.head = None        
+        self.head = None
+
+    def add(self,data):
+        NewNode = node(data)
+        if self.head is None:
+            self.head = NewNode
+        else:
+            cur = self.head
+
+            while(cur.pointer is not None):
+                cur = cur.pointer
+            cur.pointer = NewNode                    
