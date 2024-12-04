f5f773fb4332305797e3c28609ae0b66b9c2e85a
sharan376002 <sharang.developer@gmail.com>
2024-12-04 23:11:08 +0530

    linkedlist

---
+++ linkedlistOperation.py
@@ -0,0 +1,9 @@
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.pointer  = None
+
+
+class linkedlistOpertation:
+    def __init__(self):
+        self.head = None        
