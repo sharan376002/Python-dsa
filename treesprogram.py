6cd75bebdfcd7b2c93c4cc0e2a6c0255187ace1b
sharan376002 <sharang.developer@gmail.com>
2024-12-07 00:49:24 +0530

    trees

--- treesprogram.py
+++ treesprogram.py
@@ -29,6 +29,23 @@ class Tree:
     def findparentnode(self,par_data,node):
         if node.data == par_data:
             return node
-        
-            
+
+        for child in node.children:
+            nodefound = self.findparentnode(par_data, child)
+            if not nodefound:
+                return nodefound
+
+        return None        
+
+
+
+
+t1 = Tree()
+t1.Add(1)
+t1.Add(2,1)
+t1.Add(3,1)
+t1.Add(4,2)
+t1.Add(5,3)
+t1.Add(6,4)
+t1.Add(7,4)
 
