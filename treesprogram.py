74ef37c158d7ba7bfc08004bf71b84495bd66ef9
sharan376002 <sharang.developer@gmail.com>
2024-12-06 23:53:47 +0530

    trees

--- treesprogram.py
+++ treesprogram.py
@@ -4,3 +4,11 @@ class TreeNode:
     def __init__(self, data):
         self.data = data
         self.children = []
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def Add(self,data,parentdata=None):
+        Node = TreeNode(data)    
