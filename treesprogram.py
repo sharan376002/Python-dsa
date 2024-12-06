82e7ef5ad0a28ae774481a0e16dcb55466c33b06
sharan376002 <sharang.developer@gmail.com>
2024-12-07 00:42:20 +0530

    trees

--- treesprogram.py
+++ treesprogram.py
@@ -11,4 +11,24 @@ class Tree:
         self.root = None
 
     def Add(self,data,parentdata=None):
-        Node = TreeNode(data)    
+        Node = TreeNode(data)
+
+        if not self.root:  # is self.root is not none
+            self.root = Node
+            return
+        parentnode = self.findparentnode(parentdata,self.root)
+        if not parentnode:
+            print("parentnode is not found")
+            return
+
+
+        parentnode.children.append(Node)    
+
+
+
+    def findparentnode(self,par_data,node):
+        if node.data == par_data:
+            return node
+        
+            
+
