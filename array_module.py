7391fbc6d5c23a816c8a05e977ae1521a68a9a76
sharan376002 <sharang.developer@gmail.com>
2024-12-09 00:33:31 +0530

    delete unwanted

--- array_module.py
+++
@@ -1,9 +0,0 @@
-import array
-from array import *
-import array as ar
-
-arr =ar.array('i',[1,2,3,4,4,5,5])
-print(arr)
-
-# thia is the array module used inpython  that single quotes are type codes like data types
-
