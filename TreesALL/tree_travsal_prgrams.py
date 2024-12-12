34b26df925ab6c537c9cd0a2385ac01b7bded577
sharan376002 <sharang.developer@gmail.com>
2024-12-12 23:55:51 +0530

    travasl techniques

---
+++ TreesALL/tree_travsal_prgrams.py
@@ -0,0 +1,12 @@
+
+# before seeing the coding see the txt file
+class BSTnode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
