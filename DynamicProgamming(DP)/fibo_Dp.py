467aa4601d0695cbc7df8de9a943eb92ea110ac5
sharan376002 <sharang.developer@gmail.com>
2025-01-14 00:29:35 +0530

    dynammic progamming

--- DynamicProgamming(DP)/fibo_Dp.py
+++ DynamicProgamming(DP)/fibo_Dp.py
@@ -12,4 +12,14 @@ def fibo(position):
 
     print(f"fibo{position} is {dp[-1]}")    
 
+#top down approach
+
+def fibodw(position,memory={}):
+    if position in  memory:
+        return memory[position]
+    if position<=2:
+        return 1
     
+    memory[position] = fibodw(position-1,memory) + fibodw(position-2,memory)
+
+    return memory[position] 
