4d96d7a10c8c1817d93b629ed32c5fe3680c75d4
sharan376002 <sharang.developer@gmail.com>
2025-01-19 23:57:12 +0530

    hashmap coding

--- hashmap/hashmapCode.py
+++ hashmap/hashmapCode.py
@@ -7,14 +7,9 @@ class HashMap:
     def getIndex(self,key):
         return hash(key) % self.size  
 
-    def __setitem__(self,key,value):
-        index = self.getIndex(key)
-
-        if self.hashlist[index] is None:
-            self.hashlist[index] = [[key,value]]
+ 
 
 
 
 
 
-            
