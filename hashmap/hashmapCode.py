870bea5d301cf68cedfc99d9ad7a47316c31a2a8
sharan376002 <sharang.developer@gmail.com>
2025-01-21 17:18:40 +0530

    hashmap coding

--- hashmap/hashmapCode.py
+++ hashmap/hashmapCode.py
@@ -21,7 +21,18 @@ class HashMap(dict):
             for pairs in sublist:
                 if pairs[0]==key:
                     return pairs[1]
-            return "key not found"        
+            return "key not found"    
+
+    def __delitem__(self, key):
+        index = self.getIndex(key)
+        if self.hashlist[index]:
+            sublist = self.hashlist[index]
+            for i , pairs in enumerate(sublist):
+                if pairs[0] == key:
+                    del sublist[i]
+                    return
+            
+        return 'key not found'    
 
 
 
