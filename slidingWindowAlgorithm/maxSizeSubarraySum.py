d1840aeaf06cf10b3b80bb02f54afaf69b58a0e3
sharan376002 <sharang.developer@gmail.com>
2025-01-25 20:10:07 +0530

    siliding window problemm#leetcode problem

--- slidingWindowAlgorithm/maxSizeSubarraySum.py
+++ slidingWindowAlgorithm/maxSizeSubarraySum.py
@@ -10,12 +10,12 @@ right = 0
 mini = []
 subsum = 0
 while right<len(nums):
-    subsum +=nums(right)
+    subsum +=nums[right]
 
     while subsum >= target:
         mini.append(right-left+1)
-        subsum -= nums(left)
+        subsum -= nums[left]
         left +=1
     right +=1
     
-0 if len(mini)==0 else min(mini)
+print(0 if len(mini)==0 else min(mini))
