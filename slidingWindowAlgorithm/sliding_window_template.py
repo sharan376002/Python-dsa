d96f71042bfd786df5c6f82270bb4e6b461a712c
sharan376002 <sharang.developer@gmail.com>
2026-02-28 19:50:10 +0530

    Trying_to_get_good_at_sliding_window

---
+++ slidingWindowAlgorithm/sliding_window_template.py
@@ -0,0 +1,16 @@
+left = 0
+for right in range(len(arr)):
+    
+    # expand window
+    add(arr[right])
+    
+    # shrink while invalid
+    while window_invalid:
+        remove(arr[left])
+        left += 1
+    
+    # update result
+    result = max(result, right - left + 1)
+
+
+    # very import template !!!! 
